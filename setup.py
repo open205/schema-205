1b11ecd9dd2b24bb8a42df5a2d43f6e78dafed95
Neal Kruis <neal.kruis@bigladdersoftware.com>
2020-07-29 10:12:42 -0600

    Move dependecy packages to install requirements.

--- setup.py
+++ setup.py
@@ -7,5 +7,10 @@ os.environ['PBR_VERSION'] = '0.1.0' # Stop-gap until https://bugs.launchpad.net/
 
 setup(
     setup_requires=['pbr'],
+    install_requires=[
+        'jsonschema',
+        'pyyaml',
+        'pytablewriter'
+    ],
     pbr=True
 )
