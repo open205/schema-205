81bdcd6c758e1a71dae7e4b2dd3f3b1aab6a2151
tanaya-bigladder <57679220+tanaya-bigladder@users.noreply.github.com>
2020-10-21 16:51:00 -0600

    Add separate base classes for functional elements relating to performance map.

---
+++ schema205/src/grid_variables_base.h
@@ -0,0 +1,29 @@
+#ifndef LOOKUP_VARIABLES_BASE_H_
+#define LOOKUP_VARIABLES_BASE_H_
+
+#include <memory>
+#include <vector>
+#include <iostream>
+
+class performance_map_base;
+
+// ------------------------------------------------------------------------------------------------
+/// @class grid_variables_base grid_variables_base.h
+
+class grid_variables_base {
+
+public:
+    grid_variables_base() = default;
+    virtual ~grid_variables_base() = default;
+    grid_variables_base(const grid_variables_base& other) = default;
+    grid_variables_base& operator=(const grid_variables_base& other) = default;
+
+    virtual void Populate_performance_map(const performance_map_base* performance_map);
+
+    inline void Add_grid_axis(const performance_map_base* performance_map, std::vector<double>& axis)
+    {
+        std::cout << "Adding grid axis with size " << axis.size() << "\n";
+    }
+};
+
+#endif
--- schema205/src/performance_map_base.h
+++ schema205/src/performance_map_base.h
@@ -1,17 +1,23 @@
 #ifndef PERFORMANCE_MAP_BASE_H_
 #define PERFORMANCE_MAP_BASE_H_
 
+#include <memory>
+#include <vector>
+#include <iostream>
 
+// ------------------------------------------------------------------------------------------------
 /// @class performance_map_base performance_map_base.h
 
 class performance_map_base {
 
 public:
-
     performance_map_base() = default;
     virtual ~performance_map_base() = default;
     performance_map_base(const performance_map_base& other) = default;
     performance_map_base& operator=(const performance_map_base& other) = default;
+
+    //private btwxt instance to collect grid and tables for a single performance map
+    //public Calculate_performance(target)
 };
 
 #endif
