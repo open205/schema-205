cb611b8e0a0b9729ae15e16edf84df86b61ebde8
tanaya-bigladder <57679220+tanaya-bigladder@users.noreply.github.com>
2021-10-19 15:46:09 -0600

    Update target file list.

--- dodo.py
+++ dodo.py
@@ -31,12 +31,13 @@ def collect_target_files(target_dir, extension):
   return file_list
 
 def collect_lib_target_files(target_dir, extension):
-  file_list = []
+  file_list = [os.path.join(target_dir,'libtk205.h'), 
+               os.path.join(target_dir,'libtk205.cpp')]
   for file_name in sorted(os.listdir('schema-source')):
     if '.schema.yaml' in file_name:
       file_name_root = os.path.splitext(os.path.splitext(file_name)[0])[0]
       file_list.append(os.path.join(target_dir,f'{file_name_root}.{extension}'))
-      file_list.append(os.path.join(target_dir,f'{file_name_root}_factory.{extension}'))
+      #file_list.append(os.path.join(target_dir,f'{file_name_root}_factory.{extension}'))
   return file_list
 
 def task_validate():
@@ -108,9 +109,10 @@ def task_cpp():
   '''Generates CPP source files from common-schema'''
   return {
     'file_dep': collect_source_files(),
-    'targets': collect_lib_target_files(CPP_PATH,'cpp'),
+    'targets': collect_lib_target_files(HEADER_PATH,'h') + collect_lib_target_files(CPP_PATH,'cpp'),
     'task_dep': ['validate'],
     'actions': [
+      (create_folder, [HEADER_PATH]),
       (create_folder, [CPP_PATH]),
       (schema205.cpp_translate.translate_all_to_source,[SOURCE_PATH, HEADER_PATH, CPP_PATH, "ASHRAE205"])
       ],
