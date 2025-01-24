bbe640f681e2c7a44a20d28d5b68b9906d061e74
Tanaya Mankad <tanaya.mankad@bigladdersoftware.com>
2025-01-24 12:08:09 -0700

    First attempts at lattice integration.

--- dodo.py
+++ dodo.py
@@ -1,13 +1,18 @@
+import os
+from pathlib import Path
 import schema205.validate
 import schema205.markdown
-import schema205.json_translate
-import schema205.cpp_translate
+#import schema205.json_translate
+#import schema205.cpp_translate
 import schema205.render_template
-import os
 from doit.tools import create_folder
 from schema205.util import snake_style
+from lattice import Lattice
+from lattice.cpp.header_entry_extension_loader import load_extensions
+
 
 BUILD_PATH = os.path.join(os.path.dirname(__file__), 'build')
+create_folder(BUILD_PATH)
 SOURCE_PATH = os.path.join(os.path.dirname(__file__), 'schema-source')
 DOCS_PATH = os.path.join(BUILD_PATH,"docs")
 SCHEMA_PATH = os.path.join(BUILD_PATH,"schema")
@@ -17,11 +22,8 @@ RENDERED_TEMPLATE_PATH = os.path.realpath(
         os.path.join(BUILD_PATH,"rendered_template"))
 
 def collect_source_files():
-  file_list = []
-  for file_name in sorted(os.listdir('schema-source')):
-    if '.schema.yaml' in file_name:
-      file_list.append(os.path.join(SOURCE_PATH,file_name))
-  return file_list
+  source_files = sorted((Path(SOURCE_PATH, 'schema')).glob("*.schema.y*ml"))
+  return source_files
 
 def collect_cpp_generators():
   return [os.path.join('schema205', generator_py) for generator_py in ['cpp_entries.py', 'header_entries.py', 'cpp_translate.py']]
@@ -43,12 +45,7 @@ def collect_lib_target_files(target_dir, extension):
       file_list.append(os.path.join(target_dir,f'{file_name_root}_factory.{extension}'))
   return file_list
 
-def task_validate():
-  '''Validates source-schema against meta-schema'''
-  return {
-    'file_dep': [os.path.join("meta-schema","meta.schema.json")] + collect_source_files(),
-    'actions': [(schema205.validate.validate_dir,[SOURCE_PATH])]
-  }
+data_model_205 = Lattice(SOURCE_PATH, BUILD_PATH, build_output_directory_name=None, build_validation=False)
 
 def task_doc():
   '''Generates Markdown tables from source-schema'''
@@ -60,7 +57,7 @@ def task_doc():
         os.path.join('schema205','md','grid_table.py'),
         ],
     'targets': collect_target_files(DOCS_PATH,'md'),
-    'task_dep': ['validate'],
+    'task_dep': ['validate_schemas'],
     'actions': [
       (create_folder, [DOCS_PATH]),
       (schema205.markdown.write_dir,[SOURCE_PATH, DOCS_PATH])
@@ -86,7 +83,7 @@ def task_render_template():
               os.path.join('schema205', 'render_template.py'),
               ],
           'targets': [out_file, log_file],
-          'task_dep': ['validate'],
+          'task_dep': ['validate_schemas'],
           'actions': [
               (create_folder, [RENDERED_TEMPLATE_PATH]),
               (schema205.render_template.main,
@@ -95,15 +92,20 @@ def task_render_template():
           'clean': True,
           }
 
-def task_schema():
-  '''Generates JSON schema from source-schema'''
+def task_validate_schemas():
+    """Validate the data model schemas against the JSON meta schema"""
+    return {
+      'file_dep': collect_source_files(),
+      'actions': [(data_model_205.validate_schemas, [])]
+    }
+
+def task_generate_meta_schemas():
+  '''Generates JSON meta-schema from source-schema'''
   return {
-    'file_dep': [os.path.join('schema205', 'json_translate.py')] + collect_source_files(),
+    'file_dep': collect_source_files(),
     'targets': collect_target_files(SCHEMA_PATH,'json'),
-    'task_dep': ['validate'],
     'actions': [
-      (create_folder, [SCHEMA_PATH]),
-      (schema205.json_translate.translate_dir,[SOURCE_PATH, SCHEMA_PATH])
+      (data_model_205.generate_meta_schemas,[])
       ],
     'clean': True
   }
@@ -113,18 +115,17 @@ def task_cpp():
   return {
     'file_dep': collect_source_files() + collect_cpp_generators(),
     'targets': collect_lib_target_files(HEADER_PATH,'h') + collect_lib_target_files(CPP_PATH,'cpp'),
-    'task_dep': ['validate'],
+    'task_dep': ['validate_schemas'],
     'actions': [
-      (create_folder, [HEADER_PATH]),
-      (create_folder, [CPP_PATH]),
-      (schema205.cpp_translate.translate_all_to_source,[SOURCE_PATH, HEADER_PATH, CPP_PATH, "tk205"])
+      (load_extensions, [Path(SOURCE_PATH, "cpp", "extensions")]),
+      (data_model_205.generate_cpp_project,[SOURCE_PATH, HEADER_PATH, CPP_PATH, "tk205"])
       ],
     'clean': True
   }
 
-def task_test():
-  '''Performs unit tests and example file validation tests'''
-  return {
-    'task_dep': ['schema'],
-    'actions': ['pytest -v test']
-  }
+# def task_test():
+#   '''Performs unit tests and example file validation tests'''
+#   return {
+#     'task_dep': ['schema'],
+#     'actions': ['pytest -v test']
+#   }
---
+++ schema-source/cpp/extensions/lookup_struct.py
@@ -0,0 +1,42 @@
+import re
+import copy
+from dataclasses import dataclass
+from lattice.cpp.header_entries import DataElement, HeaderEntry, Struct
+# from lattice.cpp.header_entries import register_data_group_operation
+from lattice.cpp.header_translator import PluginInterface
+
+@dataclass
+class LookupStruct(HeaderEntry):
+    """
+    Special case struct for Lookup Variables. Its str overload adds a LookupStruct declaration.
+    """
+    def __post_init__(self):
+        super().__post_init__()
+        self.name = f"{self.name}Struct"
+        self.type = "struct"
+        self._closure = "};"
+
+        self.trace()
+
+    def __str__(self):
+        # Add a LookupStruct that offers a SOA access rather than AOS
+        struct = f"{self._indent}{self.type} {self.name} {self._opener}\n"
+        for c in [ch for ch in self.child_entries if isinstance(ch, DataElement)]:
+            m = re.match(r'std::vector\<(.*)\>', c.type)
+            assert m is not None
+            struct += f"{self._indent}\t{m.group(1)} {c.name};\n"
+        struct += f"{self._indent}{self._closure}"
+
+        return struct
+
+
+class LookupStructPlugin(PluginInterface, base_class="LookupVariablesTemplate"):
+    """"""
+    def process_data_group(self, parent_node: HeaderEntry):
+        for entry in parent_node.child_entries:
+            if isinstance(entry, Struct) and entry.superclass == "ashrae205::LookupVariablesTemplate":
+                ls = LookupStruct(entry.name, entry.parent)
+                for child in [ch for ch in entry.child_entries if isinstance(ch, DataElement)]:
+                    ls._add_child_entry(copy.deepcopy(child))
+            else:
+                self.process_data_group(entry)
